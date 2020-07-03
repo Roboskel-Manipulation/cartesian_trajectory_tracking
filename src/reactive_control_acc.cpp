446135e131d5480606797c8868ce5ebc10cc3b49
Thanasists <thtsitos@gmail.com>
2020-07-03 18:03:56 +0300

    minor changes in double integration

--- src/reactive_control_acc.cpp
+++ src/reactive_control_acc.cpp
@@ -36,14 +36,14 @@ void human_motion_callback(const geometry_msgs::PointStampedConstPtr human_msg){
 		keypoint_time = ros::Time::now();
 		robot_state->header.stamp = timenow;
 		state_pub_low_f.publish(*robot_state);
-	  	vis_robot_pub.publish(*marker_robot);
+		vis_robot_pub.publish(*marker_robot);
 	  	// ROS_INFO("Num of low freq robot state: %d", count);
 		if (temp_z > 10){
-			dis.data = sqrt(pow(desired_robot_position->point.x - robot_state->point.x, 2) 
-				+ pow(desired_robot_position->point.y - robot_state->point.y, 2));
+			dis.data = sqrt(pow(desired_robot_position->point.x - robot_state->pose.position.x, 2) 
+				+ pow(desired_robot_position->point.y - robot_state->pose.position.y, 2));
 			dis_pub.publish(dis);
 		}
-		marker_robot->points.push_back(robot_state->point);
+		marker_robot->points.push_back(robot_state->pose.position);
 		init_point = true;
 		if (count == 1){
 			time_duration = keypoint_time.toSec() - time_init;
@@ -79,9 +79,9 @@ void human_motion_callback(const geometry_msgs::PointStampedConstPtr human_msg){
     marker_human->points.push_back(desired_robot_position->point);
   	vis_human_pub.publish(*marker_human);
 	if (var){
-		v1->push_back(robot_state->point.x);
-		v1->push_back(robot_state->point.y);
-		v1->push_back(robot_state->point.z);
+		v1->push_back(robot_state->pose.position.x);
+		v1->push_back(robot_state->pose.position.y);
+		v1->push_back(robot_state->pose.position.z);
 		v2->push_back(desired_robot_position->point.x);
 		v2->push_back(desired_robot_position->point.y);
 		v2->push_back(desired_robot_position->point.z);
@@ -100,13 +100,13 @@ void human_motion_callback(const geometry_msgs::PointStampedConstPtr human_msg){
 }
 
 void state_callback (const trajectory_execution_msgs::PoseTwist::ConstPtr state_msg){
-	robot_state->point.x = state_msg->pose.position.x;
-	robot_state->point.y = state_msg->pose.position.y;
-	robot_state->point.z = state_msg->pose.position.z;
+	robot_state->pose.position.x = state_msg->pose.position.x;
+	robot_state->pose.position.y = state_msg->pose.position.y;
+	robot_state->pose.position.z = state_msg->pose.position.z;
 	robot_state->header.stamp = ros::Time::now();
 
 	vel_duration = ros::Time::now().toSec() - robot_velocity->header.stamp.toSec();
-	
+	std::cout << vel_duration << std::endl;
 	robot_velocity->twist.linear.x = state_msg->twist.linear.x;
 	robot_velocity->twist.linear.y = state_msg->twist.linear.y;
 	robot_velocity->twist.linear.z = state_msg->twist.linear.z;
@@ -115,9 +115,9 @@ void state_callback (const trajectory_execution_msgs::PoseTwist::ConstPtr state_
 
 	if (received_point){
 		if (count == 1){
-			vel_control->linear.x = (desired_robot_position->point.x - robot_state->point.x);
-			vel_control->linear.y = (desired_robot_position->point.y - robot_state->point.y);
-			vel_control->linear.z = (desired_robot_position->point.z - robot_state->point.z);
+			vel_control->linear.x = (desired_robot_position->point.x - robot_state->pose.position.x);
+			vel_control->linear.y = (desired_robot_position->point.y - robot_state->pose.position.y);
+			vel_control->linear.z = (desired_robot_position->point.z - robot_state->pose.position.z);
 			vel_control->angular.x = 0;
 			vel_control->angular.y = 0;
 			vel_control->angular.z = 0;
@@ -125,18 +125,18 @@ void state_callback (const trajectory_execution_msgs::PoseTwist::ConstPtr state_
 		}
 		else{
 			// std::cout << K << " " << D << std::endl;
-			acc[0] = K*(desired_robot_velocity->linear.x - robot_velocity->twist.linear.x) + D*(desired_robot_position->point.x - robot_state->point.x);
-			acc[1] = K*(desired_robot_velocity->linear.y - robot_velocity->twist.linear.y) + D*(desired_robot_position->point.y - robot_state->point.y);
-			acc[2] = K*(desired_robot_velocity->linear.z - robot_velocity->twist.linear.z) + D*(desired_robot_position->point.z - robot_state->point.z);
-			vel_control->linear.x = robot_velocity->twist.linear.x + acc[0]*time_duration;
-			vel_control->linear.y = robot_velocity->twist.linear.y + acc[1]*time_duration;
-			vel_control->linear.z = robot_velocity->twist.linear.z + acc[2]*time_duration;
-			std::cout << *vel_control << std::endl;
+			acc[0] = K*(desired_robot_velocity->linear.x - robot_velocity->twist.linear.x) + D*(desired_robot_position->point.x - robot_state->pose.position.x);
+			acc[1] = K*(desired_robot_velocity->linear.y - robot_velocity->twist.linear.y) + D*(desired_robot_position->point.y - robot_state->pose.position.y);
+			acc[2] = K*(desired_robot_velocity->linear.z - robot_velocity->twist.linear.z) + D*(desired_robot_position->point.z - robot_state->pose.position.z);
+			vel_control->linear.x = robot_velocity->twist.linear.x + acc[0]*vel_duration;
+			vel_control->linear.y = robot_velocity->twist.linear.y + acc[1]*vel_duration;
+			vel_control->linear.z = robot_velocity->twist.linear.z + acc[2]*vel_duration;
+			// std::cout << *vel_control << std::endl;
 			pub.publish(*vel_control);
 		}
-		if (abs(robot_state->point.x - init_x) < 0.005
-		 and abs(robot_state->point.y - init_y) < 0.005 
-		 and abs(robot_state->point.z - init_z) < 0.005
+		if (abs(robot_state->pose.position.x - init_x) < 0.005
+		 and abs(robot_state->pose.position.y - init_y) < 0.005 
+		 and abs(robot_state->pose.position.z - init_z) < 0.005
 		 and not init_point){
 		 	ROS_INFO("Reached the first point");
 		}
@@ -206,7 +206,7 @@ int main(int argc, char** argv){
 
 	pub = n.advertise<geometry_msgs::Twist>(ee_vel_command_topic, 100);
 	state_pub_high_f = n.advertise<trajectory_execution_msgs::PoseTwist>("/ee_position_high_f", 100);
-	state_pub_low_f = n.advertise<geometry_msgs::PointStamped>("/ee_position_low_f", 100);
+	state_pub_low_f = n.advertise<geometry_msgs::PoseStamped>("/ee_position_low_f", 100);
 	dis_pub = n.advertise<std_msgs::Float64>("/response_topic", 100);
 	vis_human_pub = n.advertise<visualization_msgs::Marker>("/vis_human_topic", 100);
 	vis_robot_pub = n.advertise<visualization_msgs::Marker>("/vis_robot_topic", 100);
